2e5527187a25ddeaa89e2de275df64f7f7a03f74
Satya Mehta <satya.h.mehta@gmail.com>
2022-02-05 13:04:29 -0500

    completed hw

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // created modules
-const employeeMod = require("./lib/Employee");
 const managerMod = require("./lib/Manager");
 const engineerMod = require("./lib/Engineer");
 const internMod = require("./lib/Intern");
 const questions = require("./lib/Questions");
-const generateHTML = require('./lib/generateHtml')
+const generateHTML = require("./lib/generateHtml");
 
+// holds new employee bojct
 const newEmployees = [];
 
 // function that prompts the user to generate the employees
 const createManager = () => {
+// prompt to create an intern, and then push the intern object to the newEmployees array
   const createIntern = () => {
     inquirer
       .prompt(questions.questionsIntern)
@@ -23,6 +24,7 @@ const createManager = () => {
         menu();
       });
   };
+// promt to create an engineer, and then push the intern object to the newEmployees array
   const createEngineer = () => {
     inquirer
       .prompt(questions.questionsEngineer)
@@ -32,52 +34,45 @@ const createManager = () => {
         menu();
       });
   };
+// menu function that gives the user choices to create manager, intern or engineer. 
   const menu = () => {
-    inquirer.prompt({
-      type: "list",
-      name: "newEmployee",
-      message: "Create another profile?",
-      choices: ["Engineer", "Intern", "Done"],
-      filter(val) {
-        return val.toLowerCase();
-      },
-    })
-    .then(({newEmployee})=>{
-      switch (newEmployee) {
-        case "engineer":
-          createEngineer();
-          break;
-        case "intern":
-          createIntern();
-          break;
-        case "done":
-          console.log("employees created!");
-          // module.exports = newEmployees
-          generateHTML(newEmployees);
-          // console.log(newEmployees)
-          break;
-        default:
-          console.log("invalid option");
-      }
-    })
-    // .then(pageHTML => {
-    //   return writeFile(pageHTML)
-    // })
+    inquirer
+      .prompt({
+        type: "list",
+        name: "newEmployee",
+        message: "Create another profile?",
+        choices: ["Engineer", "Intern", "Done"],
+        filter(val) {
+          return val.toLowerCase();
+        },
+      })
+      .then(({ newEmployee }) => {
+        switch (newEmployee) {
+          case "engineer":
+            createEngineer();
+            break;
+          case "intern":
+            createIntern();
+            break;
+          case "done":
+            const htmlPageContent = generateHTML(newEmployees);
+            fs.writeFile("./dist/index.html", htmlPageContent, (err) =>
+              err
+                ? console.log(err)
+                : console.log("Successfully created index.html!")
+            );
+            return htmlPageContent;
+        }
+      });
   };
   inquirer
     .prompt(questions.questionsManager)
-    .then(({ name, id, email, officeNumber}) => {
+    .then(({ name, id, email, officeNumber }) => {
       const manager = new managerMod(name, id, email, officeNumber);
-      newEmployees.push(manager)
+      newEmployees.push(manager);
       menu();
-      
     });
-
 };
-createManager();
 
-// const writeFile = data => {
-//   fs.writeFile('./dist/index.html', data, err => {
-//     err ? console.log(err) : console.log('Your team profile has been generated!');
-//   })
-// }
+// main function that starts the whole app. 
+createManager();
